cc108cc682917a1817dcf9c61521a271c4668ea2
DonOllario <oliver.stenstrom@hotmail.com>
2022-01-16 17:13:29 +0100

    Ground Work

---
+++ CustomAsserts.Tests/PaymentAsserts.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace CustomAsserts.Tests
+{
+    internal static class CustomAssert
+    {
+        public static void StringIsEmpty(this Assert assert, string actual)
+        {
+            if (string.IsNullOrEmpty(actual))
+                return;
+
+            throw new AssertFailedException($"Expect empty string but was {actual}");
+        }
+
+        public static void CardHasCorrectNumberValue(this Assert assert, string actual)
+        {
+            string removeSpace = actual.Replace(" ", "");
+
+            if (Regex.IsMatch(actual, "[a-zA-Z]+"))
+                throw new AssertFailedException($"Card Number can only contain numbers");
+
+            if (removeSpace.Length != 16)
+                throw new AssertFailedException($"Card Number needs to contain 16 numbers");
+            
+            return;
+        }
+
+    }
+}
--- CustomAsserts/PaymentAsserts.cs
+++ CustomAsserts/PaymentAsserts.cs
@@ -1,7 +1,14 @@
-﻿namespace CustomAsserts
-{
-    public class PaymentAsserts
-    {
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+namespace CustomAsserts
+{
+    public class Payments
+    {
+        public string CardNumber { get; set; }
+
+        public Payments()
+        {
+            CardNumber = "1234 5678 9012 3456";
+        }
     }
 }
